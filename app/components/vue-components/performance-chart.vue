6138b1790e974d510a603639dc342bcff6bf1292
Mostafa <mostafahamed73@yahoo.com>
2022-04-02 15:43:06 +0200

    Chart Edits

--- app/components/vue-components/performance-chart.vue
+++ app/components/vue-components/performance-chart.vue
@@ -5,17 +5,20 @@
 </template>
 
 <script>
-import moment from "moment";
-import { use } from "echarts/core";
-import { CanvasRenderer } from "echarts/renderers";
-import { LineChart } from "echarts/charts";
+import moment from 'moment';
+import { use } from 'echarts/core';
+import { CanvasRenderer } from 'echarts/renderers';
+import { LineChart } from 'echarts/charts';
 import {
   TitleComponent,
   TooltipComponent,
   GridComponent,
   VisualMapComponent,
-} from "echarts/components";
-import VChart from "vue-echarts";
+  LegendComponent,
+  ToolboxComponent,
+} from 'echarts/components';
+import VChart from 'vue-echarts';
+import store from '../../store';
 
 use([
   CanvasRenderer,
@@ -24,106 +27,102 @@ use([
   TooltipComponent,
   GridComponent,
   VisualMapComponent,
+  LegendComponent,
+  ToolboxComponent,
 ]);
 
 export default {
-  name: "PerformanceChartComponent",
-
+  name: 'PerformanceChartComponent',
   components: {
     VChart,
   },
 
   data() {
     return {
-      chartData: [
-        {
-          date_ms: 1641772800000,
-          performance: 0.2,
-        },
-        {
-          date_ms: 1641859200000,
-          performance: 0.33,
-        },
-        {
-          date_ms: 1641945600000,
-          performance: 0.53,
-        },
-        {
-          date_ms: 1642032000000,
-          performance: 0.31,
-        },
-        {
-          date_ms: 1642118400000,
-          performance: 0.65,
-        },
-        {
-          date_ms: 1642204800000,
-          performance: 0.88,
-        },
-        {
-          date_ms: 1642291200000,
-          performance: 0.07,
-        },
-      ],
+      store: store,
+      date: store.state.date,
     };
   },
 
   computed: {
+    performances() {
+      return store.getters.allPerformances;
+    },
     initOptions() {
       return {
-        width: "auto",
-        height: "300px",
+        width: 'auto',
+        height: '500px',
       };
     },
 
     chartOptions() {
       return {
         title: {
-          text: "Team Performance Index",
-          left: "center",
+          text: 'Team Performance Index',
+          left: 'center',
         },
         tooltip: {
           trigger: 'axis',
           transitionDuration: 0,
           confine: false,
           hideDelay: 0,
-          padding: 0,
+          padding: 10,
+          backgroundColor: '#16253f',
+          textStyle: {
+            color: '#fff',
+            fontSize: 12,
+            textAlign: 'center',
+          },
+        },
+        toolbox: {
+          show: true,
+          axisPointer: {
+            label: {
+              color: '#000',
+            },
+          },
         },
         grid: {
-          left: "30px",
-          right: "12px",
-          bottom: "2px",
-          top: "6px",
+          left: '30px',
+          right: '12px',
+          bottom: '2px',
+          top: '6px',
           containLabel: true,
         },
         xAxis: {
-          type: "category",
+          type: 'category',
           showGrid: false,
           data: this.xAxisData,
-          axisLine: {
-            show: true,
-          },
-          axisTick: {
-            show: false,
-          },
-          axisLabel: {
-            fontSize: 11,
-          },
+          axisLine: { show: false },
+          axisTick: { show: true },
+          axisLabel: { fontSize: 11 },
         },
         yAxis: {
           axisLabel: { show: true },
-          axisTick: { show: true },
+          axisTick: { show: true, alignWithLabel: true },
           splitLine: { show: true },
+        },
+        visualMap: {
+          right: 10,
+          top: 20,
+          bottom: 20,
+          pieces: [
+            { min: 0, max: 50, label: '0-50', color: '#f4674c' },
+            { min: 50, max: 80, label: '50-80', color: '#fbdb10' },
+            { min: 80, label: '80-100', color: '#01984f' },
+          ],
         },
         series: [
           {
+            name: 'Team Performance Index:',
             data: this.yAxisData,
-            type: "line",
-            symbol: "circle",
+            type: 'line',
+            symbol: 'circle',
             symbolSize: 2,
-            cursor: "default",
+            cursor: 'default',
             lineStyle: {
               width: 2,
+              type: 'solid',
             },
           },
         ],
@@ -131,17 +130,21 @@ export default {
     },
 
     xAxisData() {
-      return this.chartData.map((item) => this.formatDate(item.date_ms));
+      return this.performances.map((item) => this.formatDate(item.date_ms));
     },
 
     yAxisData() {
-      return this.chartData.map((item) => +item.performance * 100);
+      return this.performances.map((item) => +item.performance * 100);
     },
+  },
+
+  created() {
+    store.dispatch('getPerformances');
   },
 
   methods: {
     formatDate(dateInMs) {
-      return moment(dateInMs).format("DD MMM YYYY");
+      return moment(dateInMs).format('DD MMM YYYY');
     },
   },
 };
